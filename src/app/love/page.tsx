cd04350257047308b06220cb113ee49617c6d353
glownight <885204655@qq.com>
2025-01-28 17:17:53 +0800

    删除多余代码

--- src/app/love/page.tsx
+++ src/app/love/page.tsx
@@ -14,7 +14,7 @@ export default function Love() {
         <div className="japanese-pattern" />
         <main className="flex flex-col gap-10 row-start-2 items-start max-w-[90%] w-full">
           <p className="quote-text w-full !p-0">
-            『唯有你的触碰，才能使我的心脏开始跳动。』"
+            『唯有你的触碰，才能使我的心脏开始跳动。』
           </p>
           <p className="testp2 w-full !p-0">
             <TypeWriter text="『你我相逢的意义就在此刻，心怀勇气奔向光亮的明天。』" />
